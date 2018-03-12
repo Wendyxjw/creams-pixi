095119aca95b04eab2caf918711e17763523117f
Wang Dong <jacob.wd8816@gmail.com>
2018-03-12 10:28:01 +0800

    - add config file

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var watchify = require("watchify");
+var tsify = require("tsify");
+var gutil = require("gulp-util");
+
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/index.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
+
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest("dist"));
+}
+
+gulp.task("default", bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
