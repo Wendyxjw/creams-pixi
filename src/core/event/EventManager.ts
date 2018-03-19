e445df4290415e670da83872642c07307d037585
徐嘉文 <jwxu@creams.io>
2018-03-19 20:10:36 +0800

    operation zoomin zoomout

--- src/core/event/EventManager.ts
+++ src/core/event/EventManager.ts
@@ -1,7 +1,7 @@
 // 绑定传入事件
-import { EventAPI ,CallbackFunc} from "./EventInterface"
+import { EventAPI, CallbackFunc } from "./EventInterface"
 import { App } from "../app/App";
-import {GraphWithIndexType} from "../common/Graph"
+import { GraphWithIndexType } from "../common/Graph"
 
 export default class EventManager implements EventAPI {
     private _app: App;
@@ -9,43 +9,46 @@ export default class EventManager implements EventAPI {
     constructor(app: App) {
         this._app = app;
     }
-    
-    onClickGraph(callback: CallbackFunc): void{
-        this._app.graphManager._graphContainer.on("click",(event)=>{
+
+    onClickGraph(callback: CallbackFunc): void {
+        this._app.graphManager._graphContainer.on("click", (event) => {
             //console.log(event)
-            callback([],{x:event.data.global.x,
-                y:event.data.global.y,
-                width:1,
-                height:1})
+            callback([], {
+                x: event.data.global.x,//触发事件位置
+                y: event.data.global.y,
+                width: 1,
+                height: 1
+            })
         })
     };
 
-    onMouseEnterShape(callback: CallbackFunc): void{
-        this._app.graphManager._graphContainer.children.forEach((item:GraphWithIndexType,index:number)=>{
+    onMouseEnterShape(callback: CallbackFunc): void {
+        this._app.graphManager._graphContainer.children.forEach((item: GraphWithIndexType, index: number) => {
             //console.log(item)
-            if(item.shapeIndex){
-                item.on('mouseover',(event)=>{
-                    callback([item.shapeIndex],{
-                        x:item.x,//todo x,y 目前是0 why？？
-                        y:item.y,
-                        width:item.width,
-                        height:item.height})
+            if (item.shapeIndex) {
+                item.on('mouseover', (event) => {
+                    callback([item.shapeIndex], {
+                        x: item.x,//todo x,y 目前是0 why？？
+                        y: item.y,
+                        width: item.width,
+                        height: item.height
+                    })
                 })
             }
         })
     };
 
-    onMouseLeaveShape(callback: CallbackFunc): void{};
+    onMouseLeaveShape(callback: CallbackFunc): void { };
 
-    onMouseDownShape(callback: CallbackFunc): void{};
+    onMouseDownShape(callback: CallbackFunc): void { };
 
-    onMouseUpShape(callback: CallbackFunc): void{};
+    onMouseUpShape(callback: CallbackFunc): void { };
 
-    onMouseDownLine(callback: CallbackFunc): void{};
+    onMouseDownLine(callback: CallbackFunc): void { };
 
-    onMouseUpLine(callback: CallbackFunc): void{};
+    onMouseUpLine(callback: CallbackFunc): void { };
 
-    onMouseDownPoint(callback: CallbackFunc): void{};
+    onMouseDownPoint(callback: CallbackFunc): void { };
 
-    onMouseUpPoint(callback: CallbackFunc): void{};
+    onMouseUpPoint(callback: CallbackFunc): void { };
 }
--- test/app.js
+++ test/app.js
@@ -7,13 +7,13 @@ $(function () {
 
     let dom = $("#operation");
     $(dom.find("span")[0]).click(function () {
-        app.operationManager.zoomIn(2)
+        app.operationManager.zoomIn(1.5)
     })
     $(dom.find("span")[1]).click(function () {
         app.operationManager.justify()
     })
     $(dom.find("span")[2]).click(function () {
-        app.operationManager.zoomOut(2)
+        app.operationManager.zoomOut(1.5)
     })
 
     let eraser=$("#eraser");
